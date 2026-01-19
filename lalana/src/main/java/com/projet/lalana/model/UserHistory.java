bfa83a616179a95dbc1d90a6a99bf9581dd417e4
Gaellane <gaellaneandriantsoa37@gmail.com>
2026-01-19 21:08:29 +0300

    rajout de user_history

---
+++ lalana/src/main/java/com/projet/lalana/model/UserHistory.java
@@ -0,0 +1,29 @@
+package com.projet.lalana.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDateTime changedAt;
+}
