6bda35c8ceeda491bef20b545d87755e71be13b3
Tandrifiniaina Dylan RAJAONARIVONY <tandrifudylan@gmail.com>
2026-01-20 09:19:12 +0300

    login

---
+++ lalana/mobile/lalana-app/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import Login from "@/views/Login.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "/login",
+    component: Login
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+});
+
+export default router;
