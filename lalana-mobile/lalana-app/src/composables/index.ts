8315cb89f9fa6345e5c6fe4ee4d192c13bf0fefd
Tandrifiniaina Dylan RAJAONARIVONY <tandrifudylan@gmail.com>
2026-02-07 15:55:45 +0300

    permissions notification

--- lalana-mobile/lalana-app/src/composables/index.ts
+++ lalana-mobile/lalana-app/src/composables/index.ts
@@ -4,3 +4,4 @@ export { useSignalements } from './useSignalements';
 export { useMap } from './useMap';
 export { useProblemes } from './useProblemes';
 export { usePhoto } from './usePhoto';
+export { useNotifications } from './useNotifications';
